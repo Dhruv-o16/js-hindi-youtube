8722492d832848f11107e45a5b51885471973202
Dhruv-o16 <79371981+Dhruv-o16@users.noreply.github.com>
2025-07-22 16:51:17 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello"); // we are using nodejs, not browser
+// code readability should be high
+
+let name = "dhruv";
+let age = 18;
+let isLoggedIn = false;
+let state;
+
+// number => 2 to power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
